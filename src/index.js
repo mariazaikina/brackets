cf911004358637f4cae92dcb838c6460e4bbdc11
mariazaikina <mashtakova93@gmail.com>
2021-08-31 00:43:46 +0400

    init brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let symbols = bracketsConfig.join('').replace(/,/g, '');
+    let stack = [];
+for (let item of str) {
+        let index = symbols.indexOf(item)
+
+        if (index % 2 === 0) {
+            
+            if (item === symbols[index + 1] && stack[stack.length - 1] === index){
+                stack.pop();
+            } else if (item === symbols[index + 1] && stack[stack.length - 1] !== index) {
+                stack.push(index);
+            }
+            else{
+                stack.push(index);
+            }
+        } 
+        else {
+            if (stack.pop() !== index-1){
+                return false;
+            }
+        }
+        
+    }
+    return stack.length === 0
 }
